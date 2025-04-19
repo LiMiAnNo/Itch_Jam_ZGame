37060b24741e8431a3fe1a8e4d20864d0f47a067
viruzen <75735994+viruzen@users.noreply.github.com>
2025-04-19 01:05:24 -0400

    basic menu added

---
+++ Assets/Scripts/MAINMENU/Menu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // for built game
+        Debug.Log("Game Quit!"); // testing in Editor
+    }
+}
