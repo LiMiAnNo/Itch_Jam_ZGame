fdc923a62f54adcaa170f82a4b0dc7b476777e43
LiMiAnNo <123137537+LiMiAnNo@users.noreply.github.com>
2025-04-19 17:02:11 -0400

    Changes To Player Animations
    - Sprite flips in appropriately depending on move direction
    
    - Death animation attached
    
    - Take damage animation attached
    
    - Take damage function and health added to player controller script
    
    - Added 2d collider to player prefab
    
    - git ignore changes
    
    - editor build changes (idk)

--- Assets/Scripts/Player Character/PlayerController.cs
+++ Assets/Scripts/Player Character/PlayerController.cs
@@ -10,23 +10,59 @@ public class PlayerController : MonoBehaviour
     Vector2 playerMovement;
     Animator animator;
 
+    int health; 
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        health = 100;
+
+    }
+
+    void takeDamage(int dmg)
+    {
+        health -= dmg;
+
+
+        animator.SetTrigger("TookDamage");
+        if(health <= 0)
+        {
+            animator.SetBool("Dead",true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-       
 
+       
 
     }
 
     private void FixedUpdate()
     {
-        //rb.MovePosition(rb.position + playerMovement * 5.0f * Time.fixedDeltaTime);
+       
+        CharacterMovement();
+    }
+
+    void flipSprite()
+    {
+        if (rb.linearVelocity.normalized.x < 0 )
+        {
+            this.transform.rotation = new Quaternion(1, 180, 0, 0);
+        }
+        else if(rb.linearVelocity.normalized.x > 0)
+        {
+            this.transform.rotation = Quaternion.identity;
+        }
+
+    }
+
+    void CharacterMovement()
+    {
+        if (health <= 0) { return; }
         playerMovement.x = Input.GetAxisRaw("Horizontal");
         playerMovement.y = Input.GetAxisRaw("Vertical");
 
@@ -36,9 +72,6 @@ public class PlayerController : MonoBehaviour
 
         animator.SetInteger("Velocity", (int)playerVelocity.magnitude);
 
-        if (playerVelocity.normalized.x < 0) {
-            this.transform.rotation = new Quaternion(1, 180, 0, 0);
-        }
-        else { this.transform.rotation =  Quaternion.identity; }
+        flipSprite(); 
     }
 }
