1947a9ccb8dee21763949cd356e611daa9bb75d4
viruzen <75735994+viruzen@users.noreply.github.com>
2025-04-20 03:12:26 -0400

    somthing ui

--- Assets/Scripts/MAINMENU/Menu.cs
+++ Assets/Scripts/MAINMENU/Menu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("GameLevel");
     }
 
     public void QuitGame()
